79a3fec32c13814dd60724ef7467b08ce3d55ffe
harel moshayof <harel.moshayof@intel.com>
2023-01-01 17:38:19 +0200

    withCredentials

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -7,13 +7,15 @@ import { ITextPrediction } from 'src/interfaces/icalc-result';
   providedIn: 'root',
 })
 export class ApiService {
-  url = 'https://embible.cs.bgu.ac.il';
+  // url = 'https://embible.cs.bgu.ac.il';
+  url = 'http://localhost:443';
   constructor(private client: HttpClient) {}
 
   calc(text: string): Observable<ITextPrediction[]> {
     const params: HttpParams = new HttpParams().set('text', text);
     return this.client.get<ITextPrediction[]>(`${this.url}/calc`, {
       params: params,
+      withCredentials: true,
     });
   }
 }
